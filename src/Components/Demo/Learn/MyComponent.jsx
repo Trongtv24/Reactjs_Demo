c0ed93d0b253fb8412d69f0423d4626b8299826e
Trongtv24 <trinhvantrong357@gmail.com>
2025-01-19 00:51:25 +0700

    Video31

--- src/Components/Demo/Learn/MyComponent.jsx
+++ src/Components/Demo/Learn/MyComponent.jsx
@@ -2,9 +2,22 @@
 
 import './styles.css'
 const MyComponent = () => {
+    // const hoidanit = "eric 1"; //string
+    // const hoidanit = 25; //number
+    // const hoidanit = true; //boolean
+    // const hoidanit = undefined;
+    // const hoidanit = null;
+    const hoidanit = [1, 2, 3]
+    // const hoidanit = {
+    //     name: "hoidanit",
+    //     age: 25
+    // }
+
     return (
         <>
-            <div> chill gay& hoidanit</div>
+            <div> chill gay& hoidanit update</div>
+            <div> {JSON.stringify(hoidanit)} & hoidanit update</div>
+            <div>{console.log("ERIC")}</div>
             <div className="child"
                 style={{ borderRadius: "10px" }}>Child</div>
         </>
