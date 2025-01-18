b1c0b73a7ca7bd252b46480cf6b5dc14faeee9cb
Trongtv24 <trinhvantrong357@gmail.com>
2025-01-18 14:41:04 +0700

    Video30

--- src/Components/Demo/Learn/MyComponent.jsx
+++ src/Components/Demo/Learn/MyComponent.jsx
@@ -1,7 +1,13 @@
 //component = html + css + js
+
+import './styles.css'
 const MyComponent = () => {
     return (
-        <div> chill gay& hoidanit</div>
+        <>
+            <div> chill gay& hoidanit</div>
+            <div className="child"
+                style={{ borderRadius: "10px" }}>Child</div>
+        </>
     );
 }
 
