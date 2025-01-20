ef7ab3f5d30688de244892ec47eecb813f3d2752
Trongtv24 <trinhvantrong357@gmail.com>
2025-01-20 17:10:56 +0700

    Video34

--- src/App.jsx
+++ src/App.jsx
@@ -6,11 +6,21 @@ import TodoData from './Components/todo/TodoData';
 
 const App = () => {
 
+  const hoidanit = "Eric Arsenal";
+  const age = 25;
+  const data = {
+    address: "hanoi",
+    country: "vietnam"
+  }
+  //{key:value}
   return (
     <div className="todo-container">
       <div className="todo-title">Todo List</div>
       <TodoNew />
-      <TodoData />
+      <TodoData
+        name={hoidanit}
+        age={age}
+        data={data} />
       <div className='todo-image'>
         <img src={reactLogo} className='logo' />
       </div>
