3df1cdcee60fd12f2cd54cdb2245bc6295e6cc87
Trongtv24 <trinhvantrong357@gmail.com>
2025-01-21 00:40:52 +0700

    Video39

--- src/Components/todo/TodoNew.jsx
+++ src/Components/todo/TodoNew.jsx
@@ -9,6 +9,7 @@ const TodoNew = (props) => {
     // addNewTodo("eric") //fire
     const handleClick = () => {
         addNewTodo(valueInput)
+        setValueInput("");
     }
     const handleOnChange = (name) => {
         setValueInput(name)
@@ -17,6 +18,7 @@ const TodoNew = (props) => {
         <div className='todo-new'>
             <input type="text"
                 onChange={(event) => handleOnChange(event.target.value)}
+                value={valueInput}
             />
             <button
                 style={{ cursor: "pointer" }}
